b67f3477f35e9c0a40bc7474ccbb382c09b65444
duykhanhtran1808 <duykhanhtran1808@gmail.com>
2020-07-20 23:43:33 +0700

    Updates

---
+++ precache-manifest.6b7debbf093a244ee33933ffc53e760f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d85f5578972de170b98e966ffa26007",
+    "url": "/hoc-react-bai1/index.html"
+  },
+  {
+    "revision": "b9e764d6a4312ac92bfc",
+    "url": "/hoc-react-bai1/static/css/main.789cbb27.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/hoc-react-bai1/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/hoc-react-bai1/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b9e764d6a4312ac92bfc",
+    "url": "/hoc-react-bai1/static/js/main.a9e4d97f.chunk.js"
+  },
+  {
+    "revision": "cda1ae4028cad1796f43",
+    "url": "/hoc-react-bai1/static/js/runtime-main.3bd20380.js"
+  }
+]);
